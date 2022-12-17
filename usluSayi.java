59c9569b87288bd28154b84cd4930b89fd698ec1
SerdarSelli <53913443+SerdarSelli@users.noreply.github.com>
2022-12-17 22:53:11 +0300

    Add files via upload

---
+++ usluSayi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class usluSayi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("üssü alınacak sayı");
+        int a = scanner.nextInt();
+        System.out.print("üs olacak sayı giriniz");
+        int b = scanner.nextInt();
+        int us=1;
+        for(int i = 1;i<=b;i++){
+            us*=a;
+            System.out.println(us);
+        }
+        System.out.println(us);
+
+    }
+}
