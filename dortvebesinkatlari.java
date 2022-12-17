90f39f216a1c4ca0017ad567e186e0fc983d0a39
SerdarSelli <53913443+SerdarSelli@users.noreply.github.com>
2022-12-17 16:24:46 +0300

    Add files via upload

---
+++ dortvebesinkatlari.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class dortvebesinkatlari {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a;
+        System.out.println("sayı gir");
+        a= scan.nextInt();
+        int sayac5=0, sayac4=0;
+
+        for(int i=1;i<=a;i*=5){
+            sayac5++;
+            System.out.println("5 in "+ sayac5 + ". kuvveti= "+i);
+        }
+
+        for(int i=1;i<=a;i*=4){
+            sayac4++;
+            System.out.println("4 ün " +sayac4+ ". kuvveti= "+i);
+        }
+    }
+}
